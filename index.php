dc64e9865211169838458bd599cd505cf196a684
jeenwoodyt <woodfetre@gmail.com>
2021-08-10 19:24:31 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+  <meta name="description" content="Download High Quality Android Games">
+  <meta name="keywords" content="dls22, pes 22, fifa 22, ppsspp">
+	<title> JW GAMING</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body class="stbody">
+       <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="">FIFA 22 PPSSPP</a>
+  <a href="pes22psp.html">PES 22 PPSSPP</a>
+  <a href="dls22.html">DLS 22</a>
+  <a href="">GTA</a>
+  <a href="">Other apk Games</a>
+  <a href="#about">About</a>
+  <a href="#contact">Social Medias</a>
+  <input type="text" placeholder="Search..">
+      </div>
+
+<h2> Welcome on The Android-Game World</h2>
+
+  <p>Welcome to Jw Gaming , Here we propose High quality Android Games To Download , Most of our games are compatible with weak perfomance phone but, you will surely get a best expierence with a best Phone.</p>
+
+  <p>Here you will find Al lot of Android Games To Download : PE
+  S, FIFA, PES PPSSPP, FIFA PPSSPP, Dream League Soccer, First Touch Soccer, Gta Vice city, Gta San Andreas, Gta 5 mods,  WWE and a lot more android games to download... </p>
+
+ <p>If you are wondering where To download Latest Android Games , you are on the right Place because here You Will find A lt Of good Android Games To Download</p>
+
+  <p>You can Select the Game you want in the navigation bar, or you can even search it!</p>
+
+  <a href=""></a>
+
+<div class="homediv">
+  <h3><b>Last Games Shared</b></h3>
+
+       <section>
+            <h5 ><a href="dls22aguerombappe.html">Dream League Soccer 2022 Android Offline+online Aguero & Mbappe Edition</a></h5>
+            <a href="dls22aguerombappe.html"><img class="bestimgbro" src="img/dls22aguerombappecover.png" alt="" width="200px"></a>
+        </section>
+
+        <section>
+            <h5> <a href="dls22messicr7.html">Dream League Soccer 2022 Android Offline+online Cr7 & PSG Stars Edition </a> </h5>
+            <a href="dls22messicr7.html"><img class="bestimgbro" src="img/dls22messineybappecr7gt360.png" alt="" width="200px" ></a>
+        </section> 
+
+        <section>
+            <h5> <a href="dls22neymarcr7.html"> Dream League Soccer 2022 Android Offline+online 350MB Neymar & cr7 Edition</a></h5>
+            <a href="dls22neymarcr7.html"><img class="bestimgbro" src="img/dls22neycr7.jpg" alt="" width="200px"> </a>
+        </section>
+
+        <section>
+            <h5><a href="dls22suarez.html">Dream League Soccer 2022 Andoid Offline 350 MB Suarez Edition</a></h5>
+            <a href="dls22suarez.html"><img class="bestimgbro" src="img/dls22suarez.png" alt="" width="200px"></a>
+        </section>
+
+</div>
+
+</body>
+</html>
